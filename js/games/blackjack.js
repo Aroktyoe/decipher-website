55abb9e98bb100696a53d07d84f17dd0fe5855ce
Climbby <franciscoaf.gaming@gmail.com>
2025-05-02 10:59:57 +0000

    Added current game variable to the top of the function

--- js/games/blackjack.js
+++ js/games/blackjack.js
@@ -1,7 +1,7 @@
 import { fetchUser, fetchHeader, fetchBalance, getCSRFToken } from "../api.js";
 import { startCooldown } from "../utils.js";
 
-let currentGame;
+window.currentGame;
 
 export async function startBlackjack(event) {
     startCooldown(event.target);
